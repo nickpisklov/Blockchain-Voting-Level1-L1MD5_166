6cc8ff88f8e157b31c323218232952dc3b70c367
nickpisklov <mykola.pisklov@nure.ua>
2022-03-04 14:18:53 +0200

    Commit

---
+++ Blockchain-Voting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blockchain_Voting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
