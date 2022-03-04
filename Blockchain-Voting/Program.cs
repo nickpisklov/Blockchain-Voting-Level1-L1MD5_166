b1a493c8bc395e89f2917a25131e66d4b5bdd673
nickpisklov <mykola.pisklov@nure.ua>
2022-03-04 14:25:12 +0200

    Transaction and some voting functional

--- Blockchain-Voting/Program.cs
+++ Blockchain-Voting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Blockchain_Voting
 {
@@ -6,7 +7,26 @@ namespace Blockchain_Voting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, int> points = new Dictionary<string, int>()
+            {
+                {"Yes", 0},
+                {"No", 0}
+            };
+            Voting voting = new Voting(1, "MyVoting", points, new DateTime(2022, 2, 22), new DateTime(2022, 2, 25));
+            Console.WriteLine(voting.ToString());
+            Console.WriteLine();
+
+            Transaction transaction = new Transaction(voting, "No");
+            voting._points["No"] += 1;
+            Transaction transaction1 = new Transaction(voting, "No");
+            voting._points["No"] += 1;
+            Transaction transaction2 = new Transaction(voting, "No");
+
+            transaction.Print();
+            Console.WriteLine();
+            transaction1.Print();
+            Console.WriteLine();
+            transaction2.Print();
         }
     }
 }
---
+++ Blockchain-Voting/Transaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Blockchain_Voting
+{
+    internal class Transaction
+    {
+        private string _id;
+        private Voting _voting;
+        private string _vote;
+        private int _nonce;
+
+        public Transaction(Voting voting, string vote)
+        {
+            _nonce = voting.GetNumberOfVotesByVoteTitle(vote);
+            _id = HashFunction.GetHashCodeForMessage($"{vote}{_nonce}{voting.ToString()}");
+            _voting = voting;
+            _vote = vote;
+        }
+
+        public override string ToString()
+        {
+            return $"Transaction ID: {_id}; Voting: {_voting.Title}; Vote: {_vote}; Nonce: {_nonce}";
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"ID - {_id};\nVoting - {_voting.Title};\nVote - {_vote};\nNonce - {_nonce}.");
+        }
+    }
+}
